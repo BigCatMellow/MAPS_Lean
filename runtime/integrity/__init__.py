fbc598d427efbab0e540d3e48b38b41cd7151ca6
BigCat Mellow <201203536+BigCatMellow@users.noreply.github.com>
2026-08-14 19:27:20 -0400

    Export run budget checks

--- runtime/integrity/__init__.py
+++ runtime/integrity/__init__.py
@@ -1,3 +1,9 @@
+from .budget import check_run_budget, write_budget_escalation
 from .git_scope import collect_git_changes, verify_git_run
 
-__all__ = ["collect_git_changes", "verify_git_run"]
+__all__ = [
+    "check_run_budget",
+    "collect_git_changes",
+    "verify_git_run",
+    "write_budget_escalation",
+]
